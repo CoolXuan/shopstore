f8e1bcb10b0a92af79e2e4a3710489267595c1b0
cx <1976756410@qq.com>
2022-08-16 22:54:15 +0800

    完成了部分css样式

--- README.md
+++ README.md
@@ -22,3 +22,71 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+# 项目分析
+vue-router
+前端路由；Key-Value键值对
+key:url 地址栏中的路径
+value:相应的组件
+注意；项目是上中下结构
+
+路由组建：
+Home首页路由组件，Search搜索框路由，Login|Register登录|注册路由
+非路由组件：
+Header，
+Footer 首页有 登录|注册页面没有
+
+
+
+<!-- flex布局用法 -->
+1. flex-direction：决定主轴的方向（即项目的排列方向）。
+
+row（默认值）：主轴为水平方向，起点在左端。
+row-reverse：主轴为水平方向，起点在右端。
+column：主轴为垂直方向，起点在上沿。
+column-reverse：主轴为垂直方向，起点在下沿。
+2. flex-wrap：默认情况下，项目都排在一条线（又称"轴线"）上。flex-wrap属性定义，如果一条轴线排不下，如何换行。
+
+nowrap（默认）：不换行。
+wrap：换行，第一行在上方。
+wrap-reverse：换行，第一行在下方。
+3. flex-flow: flex-direction || flex-wrap
+
+4. justify-content: 定义了项目在主轴上的对齐方式（横行对齐方式）。
+
+flex-start （默认值）：左对齐
+flex-end：右对齐
+center： 居中
+space-between：两端对齐，项目之间的间隔都相等。
+space-around：每个项目两侧的间隔相等。所以，项目之间的间隔比项目与边框的间隔大一倍。
+5. align-items: 属性定义项目在交叉轴上如何对齐（纵向对齐方式）。
+
+flex-start：交叉轴的起点对齐。
+flex-end：交叉轴的终点对齐。
+center：交叉轴的中点对齐。
+baseline: 项目的第一行文字的基线对齐。
+stretch（默认值）：如果项目未设置高度或设为auto，将占满整个容器的高度。
+6. align-content： 属性定义了多根轴线的对齐方式。如果项目只有一根轴线，该属性不起作用（纵向对齐方式）。
+
+flex-start：与交叉轴的起点对齐。
+flex-end：与交叉轴的终点对齐。
+center：与交叉轴的中点对齐。
+space-between：与交叉轴两端对齐，轴线之间的间隔平均分布。
+space-around：每根轴线两侧的间隔都相等。所以，轴线之间的间隔比轴线与边框的间隔大一倍。
+stretch（默认值）：轴线占满整个交叉轴。
+7. flex-grow：定义项目的放大比例，默认为0，即如果存在剩余空间，也不放大。
+
+8. flex-shrink：定义了项目的缩小比例，默认为1，即如果空间不足，该项目将缩小。
+
+9. flex-basis：定义了在分配多余空间之前，项目占据的主轴空间（main size）。
+浏览器根据这个属性，计算主轴是否有多余空间。它的默认值为auto，即项目的本来大小。
+它可以设为跟width或height属性一样的值（比如350px），则项目将占据固定空间。
+
+10. flex：是flex-grow, flex-shrink 和 flex-basis的简写，默认值为0 1 auto。后两个属性可选。
+该属性有两个快捷值：auto (1 1 auto) 和 none (0 0 auto)。
+
+11. align-self：允许单个项目有与其他项目不一样的对齐方式，可覆盖align-items属性。默认值为auto，表示继承父元素的align-items属性，如果没有父元素，则等同于stretch。
+————————————————
+版权声明：本文为CSDN博主「爱吃的喵」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/wing_1989/article/details/105479115
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,130 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="myhead">
+        <!-- 头部第一部分 网站信息 -->
+        <header class="header">
+          <div class="top">
+            <div class="container">
+              <div class="loginList">
+                <p>小陈商城欢迎您</p>
+                <p>
+                  <span>请</span>
+                  <a href="#">登录</a>
+                  <a href="#" class="register">免费注册</a>
+                </p>
+              </div>
+              <div class="typeList">
+                <a href="#">我的订单</a>
+                <a href="#">我的购物车</a>
+                <a href="#">我的商品</a>
+                <a href="#">我的会员</a>
+                <a href="#">企业采购</a>
+                <a href="#">关注小陈商城</a>
+                <a href="#">合作招商</a>
+                <a href="#">商品后台</a>
+              </div>
+            </div>
+          </div>
+          <!-- 头部第二部分 搜索栏 -->
+          <div class="bottom">
+            <h1 class="logoArea">
+              <a href="#" class="logo" title="小陈商城" target="_blank">
+                <img src="./assets/logo.png" alt="" />
+              </a>
+            </h1>
+            <div class="searcehArea">
+              <form action="###" class="searchForm">
+                <input type="text" name="" id="autocomplete" class="" />
+                <button class="btn search-btn">搜索</button>
+              </form>
+            </div>
+          </div>
+        </header>
+      </el-header>
+      <el-main class="mymain">Main</el-main>
+      <el-footer class="myfoot">Footer</el-footer>
+    </el-container>
+  </div>
 </template>
 
-<style>
+<style lang="less">
+* {
+  padding: 0;
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 12px;
+  width: 1000px;
+  height: auto;
+  margin: 0 auto;
 }
 
-nav {
-  padding: 30px;
-}
+.myhead {
+  font-weight: 600;
+  background-color: rgb(246, 246, 246);
+  margin-bottom: 10px;
+  .header {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    .bottom {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      line-height: 50px;
+      .logoArea {
+        img {
+          width: 50px;
+          height: 50px;
+        }
+      }
+    }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    .container {
+      width: 100%;
+      height: 20px;
+      line-height: 23px;
+      align-content: center;
+      background-color: rgba(128, 128, 128, 0.475);
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      justify-content: space-between;
+      a {
+        width: auto;
+        color: black;
+        font-weight: 600;
+        line-height: 20px;
+        padding: 0 5px;
+        // background-color: orange;
+        border-right: 2px solid black;
+        border-bottom: none;
+        // border-right: solid rgb(255, 255, 255) 1px;
+        list-style: none;
+        text-decoration: none;
+        position: relative;
+        text-align: center;
+        padding: 0 5px;
+      }
+      .loginList {
+        width: 25%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: space-between;
+      }
+    }
+  }
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.mymain {
+  width: 100%;
+  height: auto;
+  background-color: yellow;
+}
+.myfoot {
+  background-color: blue;
 }
 </style>
